0d3b6a39f22db01a45dfb350c23c834b30024502
fathimuhammad <79491310+fathimuhammad@users.noreply.github.com>
2021-09-09 19:21:30 +0700

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,21 @@
+import openpyxl
+from openpyxl import *
+import tkinter as tk
+
+
+def addtoexcel(namabuku, penulis, penerbit, tahunterbit):
+    wb = load_workbook(filename='D:\Kuliah\Semester 3\Pemrograman Visual\Project\database.xlsx')
+    wb.active
+    sheet = wb['InputBuku']
+    wb.save(filename='D:\Kuliah\Semester 3\Pemrograman Visual\Project\database.xlsx')
+    x = 2
+    while True:
+        if sheet['A'+ str(x)].value == None:
+            sheet['A'+ str(x)].value = namabuku
+            sheet['B'+ str(x)].value = penulis
+            sheet['C'+ str(x)].value = penerbit
+            
+            sheet['D'+ str(x)].value = tahunterbit
+            wb.save(filename='D:\Kuliah\Semester 3\Pemrograman Visual\Project\database.xlsx')
+            break
+        x+=1
